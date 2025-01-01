a17df0dd78729cfbe2cc372887e86d6690a9ff43
Arkadiusz4 <arekmika4@gmail.com>
2025-01-01 17:18:09 +0100

    Add drawing and saving to image AST tree

--- utilities/graph.py
+++ utilities/graph.py
@@ -1,8 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def add_nodes_edges(graph, parent_id, node, counter):
 
+def add_nodes_edges(graph, parent_id, node, counter):
     node_id = next(counter)
     label = f"{node.type} ({node.value})"
     graph.add_node(node_id, label=label)
@@ -12,6 +12,7 @@ def add_nodes_edges(graph, parent_id, node, counter):
 
     for child in node.children:
         add_nodes_edges(graph, node_id, child, counter)
+
 
 def generate_tree_graph(root):
     graph = nx.DiGraph()
@@ -23,6 +24,7 @@ def generate_tree_graph(root):
         add_nodes_edges(graph, root_id, child, counter)
 
     return graph
+
 
 def tree_layout(graph, root=0, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5, pos=None, parent=None):
     if pos is None:
@@ -40,6 +42,19 @@ def tree_layout(graph, root=0, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5,
     pos[root] = (xcenter, vert_loc)
     return pos
 
+
+def draw_tree_to_file(tree, file_path):
+    graph = generate_tree_graph(tree)
+    pos = tree_layout(graph)
+    labels = nx.get_node_attributes(graph, 'label')
+
+    plt.figure(figsize=(20, 20))
+    nx.draw(graph, pos, labels=labels, with_labels=True, arrows=False, node_size=2000, font_size=10)
+    plt.title("Abstract Syntax Tree (AST)")
+    plt.savefig(file_path)
+    plt.close()
+
+
 def draw_tree(tree):
     graph = generate_tree_graph(tree)
     pos = tree_layout(graph)
@@ -49,5 +64,3 @@ def draw_tree(tree):
     nx.draw(graph, pos, labels=labels, with_labels=True, arrows=False, node_size=2000, font_size=10)
     plt.title("Abstract Syntax Tree (AST)")
     plt.show()
-
-
